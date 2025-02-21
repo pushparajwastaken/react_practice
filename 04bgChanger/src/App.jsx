d9b6c20dc3e380e5bfb333eb75bd2c8a26d8b74b
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2025-02-21 17:15:31 +0530

    update 04bgchanger

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -14,36 +14,42 @@ function App() {
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 rounded-5xl">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-5xl">
           <button
+            onClick={() => setColor("red")}
             className="outline-none px-4 py-1 rounded-full text-white shadow-sm"
             style={{ backgroundColor: "red" }}
           >
             Red
           </button>
           <button
+            onClick={() => setColor("white")}
             className="outline-none px-4 py-1 rounded-full text-black shadow-sm"
             style={{ backgroundColor: "white" }}
           >
             White
           </button>
           <button
+            onClick={() => setColor("black")}
             className="outline-none px-4 py-1 rounded-full text-white shadow-sm"
             style={{ backgroundColor: "black" }}
           >
             Black
           </button>
           <button
+            onClick={() => setColor("pink")}
             className="outline-none px-4 py-1 rounded-full text-white shadow-sm"
             style={{ backgroundColor: "pink" }}
           >
             Pink
           </button>
           <button
+            onClick={() => setColor("green")}
             className="outline-none px-4 py-1 rounded-full text-white shadow-sm"
             style={{ backgroundColor: "green" }}
           >
             Green
           </button>
           <button
+            onClick={() => setColor("blue")}
             className="outline-none px-4 py-1 rounded-full text-white shadow-sm"
             style={{ backgroundColor: "blue" }}
           >
