8881ae1075b64f85e88f9928895275347e236976
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2025-09-05 08:38:58 +0530

    day 2

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,15 +1,26 @@
 import { useState } from "react";
 import Youtube from "./youtube.jsx";
+import React from "react";
+
 function App() {
   const username = "Pushparaj";
   return (
-    <>
+    <div>
       <h1>Vite React App {username}</h1>
       <h1>Vite React App</h1>
       <Youtube />
-    </>
+    </div>
   ); //you can't return two elements
   //but we can wrap all the headings in a div
 }
-
+const anotheruser = "Pushparaj";
+const Reactelement = React.createElement(
+  "a",
+  {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  `Click me to visit Google, ${anotheruser}`
+);
+export { Reactelement };
 export default App;
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,36 +1,10 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import React, { StrictMode } from "react";
+import App, { Reactelement } from "./App.jsx";
 
-function Myapp() {
-  return (
-    <div>
-      <h1>Custom App</h1>
-    </div>
-  );
-}
-// const Reactelement = {
-//   type: "a", // It's a link ("<a>")
-//   props: {
-//     href: "https://google.com", // The link should go to Google
-//     target: "_blank", // It should open in a new tab
-//   },
-//   children: "Click me to visit google", // This is the text inside the link
-// };
-
-const AnotherElement = (
-  <a href="https://google.com" target="_blank">
-    Visit Google
-  </a>
-);
-const anotheruser = "Pushparaj";
-const Reactelement = React.createElement(
-  "a",
-  {
-    href: "https://google.com",
-    target: "_blank",
-  },
-  `Click me to visit Google, ${anotheruser}`
-);
 const root = createRoot(document.getElementById("root"));
-root.render(Reactelement);
+root.render(
+  <>
+    <App />
+    {Reactelement}
+  </>
+);
