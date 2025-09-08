158365814f61d8a3fa05994928e0a4b14b895784
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2025-09-08 19:25:31 +0530

    day 4

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,37 +1,30 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [counter, setCounter] = useState(5); //setcounter aur counter are jsut name of the variables and set counter
-  // would take in the new value of the counrter as parameter
-  //let counter = 5;
+import React from "react";
+const App = () => {
+  const [counter, setCounter] = useState(5);
   const addValue = () => {
     if (counter < 20) {
-      console.log("Button clicked", Math.random());
+      console.log("Button Clicked");
       setCounter(counter + 1);
     } else {
       return false;
     }
   };
-  const removeValue = () => {
+  const RemoveValue = () => {
     if (counter > 0) {
-      console.log("Counter Removed");
+      console.log("Button Clicked");
       setCounter(counter - 1);
     } else {
       return false;
     }
   };
   return (
-    <>
-      <h1>Pushparaj And Lavaaa</h1>
-      <h2>Counter Value: {counter}</h2>
+    <div>
+      <h1>Lavanyaa Love Youuuu</h1>
+      <h2>Counter:{counter}</h2>
       <button onClick={addValue}>Add Value</button>
-      <br />
-      <button onClick={removeValue}>Remove Value</button>
-    </>
+      <button onClick={RemoveValue}>Remove Value</button>
+    </div>
   );
-}
-
+};
 export default App;
