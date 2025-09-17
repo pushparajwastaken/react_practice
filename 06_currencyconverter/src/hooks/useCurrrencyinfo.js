e2c3ca1a2238228c54fdc96e3703ce1f632a3596
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2025-09-17 22:20:43 +0530

    late commit

---
+++ 06_currencyconverter/src/hooks/useCurrrencyinfo.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyinfo(currency) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+    console.log(data);
+  }, [currency]);
+
+  return data;
+}
+
+export default useCurrencyinfo;
