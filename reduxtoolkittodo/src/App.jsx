04266f66f9b675e0a9e7c6fed3a3703337bf49b3
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2025-09-22 09:06:04 +0530

    add reduxtoolkittodo

--- reduxtoolkittodo/src/App.jsx
+++ reduxtoolkittodo/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
       <h1>This is a crash course to Redux toolkitnp</h1>
+      <AddTodo />
+      <Todos />
     </div>
   );
 }
