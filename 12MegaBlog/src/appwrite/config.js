128158662526cdea9b0c0af9f243d266361aa823
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2025-09-24 21:05:28 +0530

    slow day

--- 12MegaBlog/src/appwrite/config.js
+++ 12MegaBlog/src/appwrite/config.js
@@ -5,10 +5,59 @@ export class Service {
   client = new Client();
   databases;
   bucket;
+
   constructor() {
     this.client
       .setEndpoint(conf.appwriteURL)
       .setProject(conf.appwriteProjectId);
+    this.databases = new Databases(this.client);
+  }
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      const post = await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
+      return post;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async updatePost(slug, { title, content, featuredImage, status, userId }) {
+    try {
+      const updatedpost = await this.databases.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        }
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument(
+        conf.appwriteDatabaseId,
+        appwriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      throw error;
+    }
   }
 }
 const service = new Service();
