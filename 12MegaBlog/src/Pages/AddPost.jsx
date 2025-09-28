f43c026350b9543932c10d499cc0251830a25b6a
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2025-09-28 21:05:53 +0530

    add pages

---
+++ 12MegaBlog/src/Pages/AddPost.jsx
@@ -0,0 +1,13 @@
+import {
+  Container as ContainerComponent,
+  postForm as PostFormComponent,
+} from "../components";
+import React from "react";
+const Login = () => {
+  return (
+    <div className="py-8">
+      <LoginComponent />
+    </div>
+  );
+};
+export default Login;
--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -4,4 +4,27 @@ import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-export { Header, Footer, Container, Logo, LogoutBtn, RTE };
+import SignUp from "./SignUp";
+import AuthLayout from "./AuthLayout";
+import Button from "./Button";
+import Input from "./Input";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import Select from "./Select";
+import postForm from "./post-form/post-form";
+export {
+  Input,
+  postForm,
+  PostCard,
+  Login,
+  Select,
+  Button,
+  AuthLayout,
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  SignUp,
+};
